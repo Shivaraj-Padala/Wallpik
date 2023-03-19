a3b32b1bfb5e99ffac50b7b41198452fab9040a9
shivaraj-padala <shivarajpadala@gmail.com>
2023-03-19 13:48:30 +0530

    1. Navbar UI issues fixes 2. Network error handling notification added 3. Div buttons changed to Button tags

--- src/app/services/token-interceptor.service.ts
+++ src/app/services/token-interceptor.service.ts
@@ -1,24 +1,35 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { environment } from 'src/environment/environment';
+import { ImageFetcherService } from './image-fetcher.service';
 
 @Injectable({
   providedIn: 'root'
 })
 
-export class TokenInterceptorService implements HttpInterceptor{
-
-  constructor() {}
+export class TokenInterceptorService implements HttpInterceptor {
+  constructor(
+    private imageFetcherService: ImageFetcherService
+  ) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const tokenizedRequest = req.clone(
       {
         setHeaders: {
-          Authorization : environment.authorizationHeader
+          Authorization: environment.authorizationHeader
         }
       }
     );
-    return next.handle(tokenizedRequest);
+    return next.handle(tokenizedRequest).pipe(catchError((error: HttpErrorResponse) => {
+      return throwError(() => {
+        if (error.status == 404) {
+          this.imageFetcherService.errorMessageHandler.next('Image not found');
+          return new Error('Image not found');
+        }
+        this.imageFetcherService.errorMessageHandler.next('Something went wrong');
+        return new Error('Something went wrong');
+      })
+    }));
   }
 }
